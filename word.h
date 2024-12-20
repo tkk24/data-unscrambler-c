864b0d01e00d2241370417ef34768bc58eeb104c
tkk24 <165603296+tkk24@users.noreply.github.com>
2024-12-20 11:10:49 -0500

    Update word.h

--- word.h
+++ word.h
@@ -6,23 +6,6 @@
 #include <stdint.h>
 #include <stdio.h>
 
-// On my honor:
-//
-// - I have not discussed the C language code in my program with
-// anyone other than my instructor or the teaching assistants
-// assigned to this course.
-//
-// - I have not used C language code obtained from another student,
-// or any other unauthorized source, either modified or unmodified.
-//
-// - If any C language code or documentation used in my program
-// was obtained from an allowed source, such as a text book or course
-// notes, that has been clearly noted with a proper citation in
-// the comments of my program.
-//
-// Timothy Kim
-
-
 // struct Word - Represents a single word with an offset.
 //     word: Pointer to the string representing the word.
 //     offset: An uint16_t that stores the word's offset.
